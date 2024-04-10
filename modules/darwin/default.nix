cd26cb28978778d15d8c557272b5b64d6c471d3d
347Online | Katie Janzen <katiejanzen@347online.me>
2024-04-10 10:07:07 -0500

    Additional defaults configs

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -10,35 +10,78 @@
 
   security.pam.enableSudoTouchIdAuth = true;
 
-  system.defaults = {
-    dock = {
-      autohide = true;
-      show-recents = false;
-      persistent-apps = [
-        # System Apps
-        "/System/Applications/App Store.app"
-        "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
-        "/System/Applications/Music.app"
-        "/System/Applications/Messages.app"
-
-        # Dev Tools
-        "/${pkgs.vscodium}/Applications/VSCodium.app"
-        "/${pkgs.kitty}/Applications/kitty.app"
-
-        # Make system settings the rightmost app
-        "/System/Applications/System Settings.app"
-      ];
-    };
-
-    NSGlobalDomain = {
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-    };
-
+  system = {
     startup.chime = true;
+
+    defaults = {
+      dock = {
+        autohide = true;
+        show-recents = false;
+        persistent-apps = [
+          # System Apps
+          "/System/Applications/App Store.app"
+          "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
+          "/System/Applications/Music.app"
+          "/System/Applications/Messages.app"
+
+          # Dev Tools
+          "/${pkgs.vscodium}/Applications/VSCodium.app"
+          "/${pkgs.kitty}/Applications/kitty.app"
+
+          # Make system settings the rightmost app
+          "/System/Applications/System Settings.app"
+        ];
+      };
+
+      NSGlobalDomain = {
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+      };
+
+      CustomUserPreferences = {
+        "com.pilotmoon.scroll-reverser" = {
+          ReverseTrackpad = false;
+          StartAtLogin = true;
+          InvertScrollingOn = true;
+          ReverseX = true;
+        };
+
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+
+        "com.apple.Safari" = {
+          AutoOpenSafeDownloads = false;
+          IncludeDevelopMenu = true;
+          WebAutomaticSpellingCorrectionEnabled = false;
+        };
+
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+
+        "com.apple.print.PrintingPrefs" = {
+          # Automatically quit printer app once the print jobs complete
+          "Quit When Finished" = true;
+        };
+
+        "com.apple.SoftwareUpdate" = {
+          AutomaticCheckEnabled = true;
+          ScheduleFrequency = 1;
+          AutomaticDownload = 1;
+          CriticalUpdateInstall = 1;
+        };
+
+        "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+        "com.apple.ImageCapture".disableHotPlug = true;
+        "com.apple.commerce".AutoUpdate = true;
+      };
+    };
   };
 
   programs = {
