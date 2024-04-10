8c4c8e6d6e33ff7c068e5da011ed2e6719bbc92a
347Online | Katie Janzen <katiejanzen@347online.me>
2024-04-10 00:53:59 -0500

    More arrangement in Mac config

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,39 +1,33 @@
 {
   pkgs,
-  intel,
+  hostPlatform,
   username,
   ...
 }: {
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # DO NOT EDIT BELOW
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = hostPlatform;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix = {
     package = pkgs.nix;
     settings.experimental-features = "nix-command flakes";
   };
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-
-  security.pam.enableSudoTouchIdAuth = true;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform =
-    if intel
-    then "x86_64-darwin"
-    else "aarch64-darwin";
-
   nixpkgs.config = {
     allowUnsupportedSystem = true;
     allowUnfree = true;
   };
+
   users.users."${username}" = {
     name = username;
     home = "/Users/${username}";
   };
 
-  # DO NOT EDIT BELOW
   system.stateVersion = 4;
 }
