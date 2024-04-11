e4c55705bd9cae84a6e44b775f9d93eaf26a8480
347Online | Katie Janzen <katiejanzen@347online.me>
2024-04-11 17:44:07 -0500

    Convert to enableGit option
    Add macOS keychain git credential

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,16 +1,35 @@
-{...}: {
-  programs.git = {
-    enable = true;
-    delta.enable = true;
-    ignores = [
-      ".DS_Store"
-    ];
-    userName = "347Online | Katie Janzen";
-    userEmail = "katiejanzen@347online.me";
-    extraConfig = {
-      core.editor = "nvim";
-      init.defaultBranch = "main";
-    };
+{
+  config,
+  lib,
+  system,
+  util,
+  ...
+}: let
+  mac = util.isMac system;
+in {
+  options = {
+    enableGit = lib.mkEnableOption "git configuration module";
   };
-  programs.git-credential-oauth.enable = true;
+
+  config = lib.mkIf config.enableGit {
+    programs.git = {
+      enable = true;
+      delta.enable = true;
+      ignores = [
+        ".DS_Store"
+      ];
+      userName = "347Online | Katie Janzen";
+      userEmail = "katiejanzen@347online.me";
+      extraConfig = {
+        core.editor = "nvim";
+        init.defaultBranch = "main";
+        # TODO: There might be a better way to do this
+        credential.helper =
+          if mac
+          then "osxkeychain"
+          else null;
+      };
+    };
+    programs.git-credential-oauth.enable = !mac;
+  };
 }
