536c8a71b6db3d281c55c79d2c8c25de37af8239
347Online | Katie Janzen <katiejanzen@347online.me>
2024-05-15 23:03:25 -0500

    Home-manager module

---
+++ modules/home/default.nix
@@ -0,0 +1,58 @@
+{
+  pkgs,
+  username,
+  homeDirectory,
+  rust-toolchain,
+  ...
+}: {
+  # TODO: Break up into sub-modules
+  # Fenix especially good candidate for this
+
+  imports = [
+    ./dev
+    ./games
+  ];
+
+  programs.home-manager.enable = true;
+
+  home = {
+    inherit username homeDirectory;
+
+    packages = with pkgs; [
+      (fenix.${rust-toolchain}.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ])
+      rust-analyzer-nightly
+
+      (nerdfonts.override {fonts = ["JetBrainsMono"];})
+
+      # Nix
+      alejandra
+      nil
+
+      # Shell Tools
+      bat
+      eza
+
+      # Programming Languages
+      nodejs
+      python3
+
+      _1password
+
+      # GUI Apps
+      _1password-gui
+      obsidian
+      # maestral # TODO: Fix this cross-platform
+    ];
+  };
+
+  news.display = "silent";
+
+  # DO NOT EDIT BELOW
+  home.stateVersion = "23.11";
+}
