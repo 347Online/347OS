9185036e1212a42a3ce66e0bed946cc17e245245
347Online | Katie Janzen <katiejanzen@347online.me>
2024-05-15 22:06:30 -0500

    Create base flake.nix

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Katie's Nix Systems";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim-module = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-minecraft = {
+      url = "github:12Boti/nix-minecraft";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nil = {
+      url = "github:oxalica/nil";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    fenix,
+    nix-darwin,
+    home-manager,
+    nixpkgs,
+    nixvim-module,
+    nix-homebrew,
+    nix-vscode-extensions,
+    ...
+  }: {
+    darwinConfigurations."Athena" = {};
+    darwinConfigurations."Alice" = {};
+  };
+}
