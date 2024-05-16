b8f617c27138dd17ddeed9bc31739a8aae0a325a
347Online | Katie Janzen <katiejanzen@347online.me>
2024-05-15 22:09:01 -0500

    Copy over homebrew module, already enable option

---
+++ darwin/homebrew.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  config,
+  username,
+  ...
+}: {
+  options = {
+    enableHomebrew = lib.mkEnableOption "homebrew setup";
+  };
+
+  config = lib.mkIf config.enableHomebrew {
+    nix-homebrew = {
+      enable = true;
+      user = username;
+      autoMigrate = true;
+    };
+
+    homebrew = {
+      enable = true;
+
+      onActivation = {
+        autoUpdate = true;
+        upgrade = true;
+      };
+
+      masApps = {
+        "1Password for Safari" = 1569813296;
+        "Noir" = 1592917505;
+        "Magic Lasso" = 1198047227;
+        "GoodLinks" = 1474335294;
+        "Numbers" = 409203825;
+        "Pages" = 409201541;
+        # "Overcast" = 888422857; # Currently broken
+      };
+
+      casks = [
+        "raycast"
+        "setapp"
+        "scroll-reverser"
+        "logi-options-plus"
+      ];
+    };
+
+    environment.variables = {
+      HOMEBREW_PREFIX = "/opt/homebrew";
+      HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
+      HOMEBREW_REPOSITORY = "/opt/homebrew/Library/.homebrew-is-managed-by-nix";
+      PATH = "/opt/homebrew/bin:/opt/homebrew/sbin\${PATH+:$PATH}";
+      MANPATH = "/opt/homebrew/share/man\${MANPATH+:$MANPATH}:";
+      INFOPATH = "/opt/homebrew/share/info:\${INFOPATH:-}";
+    };
+  };
+}
