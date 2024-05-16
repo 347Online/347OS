70d10c4aae9d8a67179e275845d0c50e18a849a7
347Online | Katie Janzen <katiejanzen@347online.me>
2024-05-15 23:01:00 -0500

    Gaming module

---
+++ modules/games/default.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  nix-minecraft,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    gamingSetup.enable = lib.mkEnableOption "gaming setup";
+  };
+
+  config = lib.mkIf config.gamingSetup.enable {
+    home.packages = with pkgs; [
+      # prismlauncher # TODO: Declare Minecraft... somehow
+    ];
+
+    # TODO: Expand this
+  };
+}
