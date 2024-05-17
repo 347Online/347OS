f41a780a3915b653e88873d5490ced84690f792b
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-05-17 15:19:41 -0500

    Break bulk of initExtra into dofiles/.config/.zshrc

--- modules/code/shell.nix
+++ modules/code/shell.nix
@@ -17,6 +17,7 @@
 
   shellIntegrations = {
     enableBashIntegration = false;
+    enableZshIntegration = false;
     enableNushellIntegration = true;
     enableFishIntegration = true;
   };
@@ -74,25 +75,10 @@ in {
         enableCompletion = true;
         syntaxHighlighting.enable = true;
         autosuggestion.enable = true;
-        initExtra = ''
+        initExtraFirst = ''
           # Powerlevel10k Zsh theme
           source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-          test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
-
-          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-          export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-
-          eval "$(fzf --zsh)"
-
-          HISTDUP=erase
-          setopt appendhistory
-          setopt sharehistory
-          setopt hist_ignore_space
-          setopt hist_ignore_all_dups
-          setopt hist_ignore_dups
-          setopt hist_save_no_dups
-          setopt hist_find_no_dups
+          ${builtins.readFile ../dotfiles/.zshrc}
         '';
         shellAliases = shellAliases // {"ls" = "${pkgs.eza}/bin/eza";};
       };
