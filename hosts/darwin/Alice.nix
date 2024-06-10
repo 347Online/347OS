89611634d6d938325c76c05e0b31c8cbfd72b2e7
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-06-10 12:41:31 -0500

    Dock apps for Alice

--- hosts/darwin/Alice.nix
+++ hosts/darwin/Alice.nix
@@ -1,4 +1,10 @@
 {username, ...}: {
+  darwin.dockApps = [
+    "/Applications/Slack.app"
+    "/Applications/Microsoft Outlook.app"
+    "/Applications/zoom.us.app"
+    "/Applications/Fantastical.app"
+  ];
   home-manager.users.${username} = {
     lang.java.enable = true;
   };
