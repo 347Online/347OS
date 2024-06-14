c48d3c57b306443d74a3ef47c204931476db3741
347Online | Katie Janzen <katiejanzen@347online.me>
2024-06-13 22:31:51 -0500

    Remove layout
    Just gonna have to settle for not being able to declare this for now

--- scripts/darwin/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-dir="$(readlink -f $(dirname $BASH_SOURCE))"
-
-echo "Installing Colemak DH..."
-sudo cp -r "$dir/files/Colemak DH.bundle" "/Library/Keyboard Layouts/Colemak DH.bundle"
-
-echo "Done!"
