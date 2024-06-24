28711a849e3f33ece1f4abed9a24e61234b2ff7c
347Online | Katie Janzen <katiejanzen@347online.me>
2024-06-24 17:03:48 -0500

    Split cmp

--- modules/nvim/plugins/default.nix
+++ modules/nvim/plugins/default.nix
@@ -36,25 +36,5 @@
         yaml = ["yamllint" "yamlfmt"];
       };
     };
-
-    cmp = {
-      enable = true;
-      settings = {
-        experimental.ghost_text = true;
-        sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "buffer";}
-        ];
-
-        autoEnableSources = true;
-
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        };
-      };
-    };
   };
 }
