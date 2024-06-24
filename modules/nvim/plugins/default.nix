95cdc76d761f3ca99f25b36ec771ebebfe0b36bf
347Online | Katie Janzen <katiejanzen@347online.me>
2024-06-24 17:04:35 -0500

    Split conform

--- modules/nvim/plugins/default.nix
+++ modules/nvim/plugins/default.nix
@@ -14,27 +14,5 @@
       enable = true;
       # indent = true;
     };
-
-    conform-nvim = {
-      enable = true;
-      formatOnSave = {
-        lspFallback = true;
-        timeoutMs = 500;
-      };
-      notifyOnError = true;
-      formattersByFt = {
-        liquidsoap = ["liquidsoap-prettier"];
-        html = [["prettierd" "prettier"]];
-        css = [["prettierd" "prettier"]];
-        javascript = [["prettierd" "prettier"]];
-        javascriptreact = [["prettierd" "prettier"]];
-        typescript = [["prettierd" "prettier"]];
-        typescriptreact = [["prettierd" "prettier"]];
-        lua = ["stylua"];
-        nix = ["alejandra"];
-        markdown = [["prettierd" "prettier"]];
-        yaml = ["yamllint" "yamlfmt"];
-      };
-    };
   };
 }
