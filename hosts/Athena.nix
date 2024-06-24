b713c9e1fb188edbea521b3ff7cd96f389a0af00
347Online | Katie Janzen <katiejanzen@347online.me>
2024-06-24 12:34:02 -0500

    Add discord to Athena dock

--- hosts/Athena.nix
+++ hosts/Athena.nix
@@ -2,6 +2,7 @@
   darwin.dock.apps = [
     "/Applications/Overcast.app"
     "/System/Applications/Messages.app"
+    "/Applications/Discord.app"
     "/Applications/Telegram.app"
     "/System/Applications/Mail.app"
     "/Applications/TeamTalk5.app"
