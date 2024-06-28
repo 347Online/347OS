c00ff4dfe4c6cf0bd8c655691f7b71551f75d3ef
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-06-28 16:21:36 -0500

    Add nvim-next shell alias

--- modules/util.nix
+++ modules/util.nix
@@ -4,15 +4,17 @@ let
       with pkgs; {
         "bash" = "${bash}/bin/bash";
         "cat" = "${bat}/bin/bat";
-        "ls" = "${pkgs.eza}/bin/eza";
-        "tree" = "${pkgs.eza}/bin/eza --tree";
-        "grep" = "${pkgs.ripgrep}/bin/rg";
-        "diff" = "${pkgs.delta}/bin/delta";
+        "ls" = "${eza}/bin/eza";
+        "tree" = "${eza}/bin/eza --tree";
+        "grep" = "${ripgrep}/bin/rg";
+        "diff" = "${delta}/bin/delta";
 
         "code" = "${vscodium}/bin/codium";
 
         "git" = "${git}/bin/git";
         "branch" = "${git}/bin/git branch --show-current";
+
+        "nvim-next" = "nix run ~/src/nix-systems#nvim";
       };
   };
 in
