82b9450a35c13538c86c568100b0ebd6e289eba6
347Online | Katie Janzen <katiejanzen@347online.me>
2024-07-02 19:31:24 -0500

    Comment out linux specific dotfiles for now

--- modules/linux/default.nix
+++ modules/linux/default.nix
@@ -1,8 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  util,
+  ...
+}: {
   imports = [
     ./cursor.nix
     ./hyprland.nix
   ];
+
+  # home.file = util.toHomeFiles ./dotfiles;
 
   home.packages = with pkgs; [
     webcord
