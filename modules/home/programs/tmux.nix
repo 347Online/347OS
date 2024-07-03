7544386c67a078bdd73c6baa7af25192c360ad7d
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-07-03 15:56:31 -0500

    Tmux config enhancements Currently a work in progress

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -1,10 +1,28 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.tmux = {
     enable = true;
     baseIndex = 1;
     mouse = true;
     shortcut = "Space";
     terminal = "xterm-256color";
-    extraConfig = '''';
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = power-theme;
+        # plugin = catppuccin;
+        # extraConfig = "set -g @catppuccin_flavour 'mocha'";
+        # plugin = pkgs.tmuxplugins.mktmuxplugin {
+        #   name = "catpuccin-tmux-dreamsofcode";
+        #   pluginname = "catpuccin-tmux-dreamsofcode";
+        #   src = inputs.catpuccin-tmux;
+        #   postinstall = ''
+        #     sed -i -e 's|''${plugin_dir}/catppuccin-selected-theme.tmuxtheme|''${tmux_tmpdir}/catppuccin-selected-theme.tmuxtheme|g' $target/catppuccin.tmux
+        #   '';
+        # };
+      }
+    ];
   };
 }
