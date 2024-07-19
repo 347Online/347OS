21601a8d9bbd0b83ef97d65706b657a8c46585f0
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-07-19 13:45:03 -0500

    Adjust telescope keybinds
    Add keybind for fuzzy find in current buffer

--- modules/shared/programs/nvim/plugins/telescope.nix
+++ modules/shared/programs/nvim/plugins/telescope.nix
@@ -7,14 +7,20 @@
   keymaps = [
     {
       mode = "n";
-      key = "<leader>p";
+      key = "<leader>ff";
       action = ":Telescope find_files<CR>";
       options.silent = true;
     }
     {
       mode = "n";
-      key = "<leader>f";
+      key = "<leader>fa";
       action = ":Telescope live_grep<CR>";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>fc";
+      action = ":Telescope current_buffer_fuzzy_find<CR>";
       options.silent = true;
     }
   ];
