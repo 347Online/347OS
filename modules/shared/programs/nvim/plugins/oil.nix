57ae62603d2407fe34e3f532ba6927ca9ddaa69c
347Online | Katie Janzen <katiejanzen@347online.me>
2024-07-21 16:36:32 -0500

    Use bindSimple for Oil keybind

--- modules/shared/programs/nvim/plugins/oil.nix
+++ modules/shared/programs/nvim/plugins/oil.nix
@@ -1,8 +1,12 @@
-{
+{util, ...}: {
   plugins.oil.settings = {
     keymaps = {
       "-" = false;
       "<BS>" = "actions.parent";
     };
   };
+
+  keymaps = [
+    (util.vim.bindSimple "n" "-" ":Oil<CR>")
+  ];
 }
