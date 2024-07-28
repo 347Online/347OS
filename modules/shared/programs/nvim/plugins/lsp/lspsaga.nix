2cb2ca3f1091d1f2ac4fe84fb7c2517a10686a0d
347Online | Katie Janzen <katiejanzen@347online.me>
2024-07-27 21:29:16 -0500

    Use l prefix for most language server actions

--- modules/shared/programs/nvim/plugins/lsp/lspsaga.nix
+++ modules/shared/programs/nvim/plugins/lsp/lspsaga.nix
@@ -13,9 +13,9 @@
   };
 
   keymaps = [
-    (util.vimBind "n" "<leader>l" ":Lspsaga hover_doc<CR>")
-    (util.vimBind "n" "<leader>a" ":Lspsaga code_action<CR>")
-    (util.vimBind "n" "<leader>r" ":Lspsaga rename<CR>")
+    (util.vimBind "n" "<leader>ll" ":Lspsaga hover_doc<CR>")
+    (util.vimBind "n" "<leader>la" ":Lspsaga code_action<CR>")
+    (util.vimBind "n" "<leader>lr" ":Lspsaga rename<CR>")
     (util.vimBind "n" "gd" ":Lspsaga goto_definition<CR>")
     (util.vimBind "n" "gt" ":Lspsaga goto_type_definition<CR>")
   ];
