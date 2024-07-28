fc5de0b5514127858c7842ddfcc9edad5ee9b71f
347Online | Katie Janzen <katiejanzen@347online.me>
2024-07-27 22:56:03 -0500

    Remove discord from Athena dock

--- hosts/Athena/default.nix
+++ hosts/Athena/default.nix
@@ -1,15 +1,10 @@
-{username, ...}: {
+{
   stylix.image = ./wp-sunset.jpg;
   darwin.dock.apps = [
     "/Applications/Overcast.app"
     "/System/Applications/Messages.app"
-    "/Applications/Discord.app"
     "/Applications/Telegram.app"
     "/System/Applications/Mail.app"
     "/Applications/TeamTalk5.app"
   ];
-
-  home-manager.users.${username} = {
-    lang.rust.toolchain = "beta";
-  };
 }
