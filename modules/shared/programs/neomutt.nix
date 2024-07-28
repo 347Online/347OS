020632f700c0a13ee5d6e7ed43d317dcbf15632f
347Online | Katie Janzen <katiejanzen@347online.me>
2024-07-28 02:36:59 -0500

    Additional neomutt and email setup

--- modules/shared/programs/neomutt.nix
+++ modules/shared/programs/neomutt.nix
@@ -1,5 +1,24 @@
 {
   programs.neomutt = {
     enable = true;
+    vimKeys = true;
+  };
+
+  accounts.email = {
+    accounts.personal = {
+      realName = "Katie Janzen";
+      userName = "katie@347online.me";
+      address = "katie@347online.me";
+      imap.host = "imap.fastmail.com";
+      smtp.host = "smtp.fastmail.com";
+      primary = true;
+      # TODO: This is NOT a good way to do this
+      # Migrate to something like sops-nix and rotate this password
+      passwordCommand = "~/.secrets/email/password-command.sh";
+      neomutt = {
+        enable = true;
+        mailboxType = "imap";
+      };
+    };
   };
 }
