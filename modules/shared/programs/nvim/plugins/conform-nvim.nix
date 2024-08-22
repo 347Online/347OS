52364ce3f02b4801764513154f4dc1c3ca68239f
Katie Janzen | 347Online <katiejanzen@347Online.me>
2024-08-22 15:50:34 -0500

    Fix deprecation warning in conform
    Use new method for mixed selection of formatters

--- modules/shared/programs/nvim/plugins/conform-nvim.nix
+++ modules/shared/programs/nvim/plugins/conform-nvim.nix
@@ -1,4 +1,42 @@
-{
+{lib, ...}: let
+  extraConfigLua =
+    lib.mkBefore
+    /*
+    lua
+    */
+    ''
+      local function first(bufnr, ...)
+        local conform = require("conform")
+        for i = 1, select("#", ...) do
+          local formatter = select(i, ...)
+          if conform.get_formatter_info(formatter, bufnr).available then
+            return formatter
+          end
+        end
+        return select(1, ...)
+      end
+    '';
+
+  prettier =
+    /*
+    lua
+    */
+    ''
+      function(bufnr)
+        return first(bufnr, "prettierd", "prettier")
+      end
+    '';
+  prettier-eslint =
+    /*
+    lua
+    */
+    ''
+      function(bufnr)
+          return { first(bufnr, "prettierd", "prettier"), "eslint_d" }
+      end
+    '';
+in {
+  inherit extraConfigLua;
   plugins.conform-nvim = {
     enable = true;
 
@@ -6,19 +44,22 @@
       lspFallback = true;
       timeoutMs = 500;
     };
+
     notifyOnError = true;
     formattersByFt = {
-      html = [["prettierd" "prettier"]];
-      css = [["prettierd" "prettier"]];
-      json = [["prettierd" "prettier"] "eslint_d"];
-      jsonc = [["prettierd" "prettier"] "eslint_d"];
-      javascript = [["prettierd" "prettier"] "eslint_d"];
-      javascriptreact = [["prettierd" "prettier"] "eslint_d"];
-      typescript = [["prettierd" "prettier"] "eslint_d"];
-      typescriptreact = [["prettierd" "prettier"] "eslint_d"];
+      html.__raw = prettier;
+      css.__raw = prettier;
+      markdown.__raw = prettier;
+
+      json.__raw = prettier-eslint;
+      jsonc.__raw = prettier-eslint;
+      javascript.__raw = prettier-eslint;
+      javascriptreact.__raw = prettier-eslint;
+      typescript.__raw = prettier-eslint;
+      typescriptreact.__raw = prettier-eslint;
+
       lua = ["stylua"];
       nix = ["alejandra"];
-      markdown = [["prettierd" "prettier"]];
       yaml = ["yamllint" "yamlfmt"];
     };
   };
