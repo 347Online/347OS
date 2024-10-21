2996a6bdb2b14dbc58214343b114917a7aebaf28
347Online | Katie Janzen <katiejanzen@347online.me>
2024-10-21 00:42:18 -0500

    More services fixes for Aspen

--- hosts/Aspen/services.nix
+++ hosts/Aspen/services.nix
@@ -1,10 +1,14 @@
 {
-  services.plex.enable = true;
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
   services.transmission = {
     enable = true;
     openRPCPort = true;
     settings = {
-      rpc-whitelist = "*";
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist-enabled = false;
     };
   };
 }
