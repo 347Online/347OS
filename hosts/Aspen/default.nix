f9d17a78917d9ccfd973a0ca1921eaf61f592015
347Online | Katie Janzen <katiejanzen@347online.me>
2024-10-21 00:52:50 -0500

    Wireguard config

--- hosts/Aspen/default.nix
+++ hosts/Aspen/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hardware-configuration.nix
     ./services.nix
+    ./wireguard.nix
   ];
 
   stylix.image = "${self}/wallpapers/desert.jpg";
---
+++ hosts/Aspen/wireguard.nix
@@ -0,0 +1,17 @@
+{
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = ["192.168.2.1/32"];
+      dns = "192.168.2.1";
+      privateKeyFile = "~/.secrets/wg/key";
+
+      peers = [
+        {
+          publicKey = "FZhZ/zhB8X34nCsYFjNDuSGqrb+OUzG7/HPyZSgsiB8=";
+          allowedIPs = ["192.168.2.1/32" "192.168.2.2/32" "0.0.0.0/0"];
+          endpoint = "wlg.duckdns.org:51820";
+        }
+      ];
+    };
+  };
+}
