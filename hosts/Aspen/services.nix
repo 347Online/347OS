d67505e14c00e0adb81ef7532f996d21beb28ffa
347Online | Katie Janzen <katiejanzen@347online.me>
2024-10-31 23:57:01 -0500

    Enable auth for transmission

--- hosts/Aspen/services.nix
+++ hosts/Aspen/services.nix
@@ -7,8 +7,10 @@
     enable = true;
     openRPCPort = true;
     settings = {
+      rpc-authentication-required = true;
       rpc-bind-address = "0.0.0.0";
       rpc-whitelist-enabled = false;
     };
+    credentialsFile = "/var/secrets/transmission.json";
   };
 }
