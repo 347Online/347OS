405bdf6c47acd080c3647f77799289261777a896
347Online | Katie Janzen <katiejanzen@347online.me>
2024-11-15 12:39:14 -0600

    Toggle for FormatOnSave

--- modules/shared/programs/nvim/plugins/conform-nvim.nix
+++ modules/shared/programs/nvim/plugins/conform-nvim.nix
@@ -1,12 +1,49 @@
-{
+{lib, ...}: rec {
+  extraConfigLua =
+    lib.mkIf (plugins.conform-nvim.enable or false)
+    # lua
+    ''
+      vim.api.nvim_create_user_command(
+        "FormatDisable",
+        function(args)
+          if args.bang then
+            vim.b.disable_autoformat = true
+          else
+            vim.g.disable_autoformat = true
+          end
+        end,
+        {
+          desc = "Disable autoformat-on-save",
+          bang = true
+        }
+      )
+      vim.api.nvim_create_user_command(
+        "FormatEnable",
+        function()
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+        end,
+        {
+          desc = "Re-enable autoformat-on-save"
+        }
+      )
+    '';
+
   plugins.conform-nvim = {
     enable = true;
 
     settings = {
-      format_on_save = {
-        lsp_fallback = true;
-        timeout_ms = 750;
-      };
+      format_on_save.__raw = ''
+        function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+
+          return {
+            timeout_ms = 750, lsp_fallback = true
+          }
+        end
+      '';
 
       notify_on_error = true;
 
