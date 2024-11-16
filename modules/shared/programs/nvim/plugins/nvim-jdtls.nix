b6ca77e82898ef9511e2f0207fee9444544033fb
347Online | Katie Janzen <katiejanzen@347online.me>
2024-11-16 00:59:29 -0600

    Tweaks to nvim-java

--- modules/shared/programs/nvim/plugins/nvim-jdtls.nix
+++ modules/shared/programs/nvim/plugins/nvim-jdtls.nix
@@ -1,6 +1,22 @@
-{homeDirectory, ...}: {
-  plugins.nvim-jdtls = {
+{
+  util,
+  isDarwin,
+  homeDirectory,
+  ...
+}: {
+  plugins.nvim-jdtls = let
+    workspace_dir = "${homeDirectory}/.local/share/nvim/jdtls-workspace";
+    configuration =
+      util.mkIfElse isDarwin
+      "${workspace_dir}/config_mac"
+      "${workspace_dir}/config_linux";
+  in {
     enable = true;
-    data = "${homeDirectory}/.local/share/jdtls-workspace";
+    data.__raw =
+      # lua
+      ''
+        '${workspace_dir}/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+      '';
+    inherit configuration;
   };
 }
