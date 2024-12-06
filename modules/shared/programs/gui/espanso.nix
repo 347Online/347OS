12702f9c560e17402373673349c8176af97d06f8
347Online | Katie Janzen <katiejanzen@347online.me>
2024-12-06 12:25:00 -0600

    Migrate snippets from Rocket Typist

--- modules/shared/programs/gui/espanso.nix
+++ modules/shared/programs/gui/espanso.nix
@@ -1,3 +1,101 @@
 {
-  services.espanso.enable = true;
+  services.espanso = {
+    enable = true;
+
+    matches = {
+      base = {
+        matches = [
+          {
+            replace = "Canyon";
+            trigger = ";ct";
+          }
+          {
+            replace = "Codesmith Software Engineering Residency for Experienced Programmers";
+            trigger = ";csl";
+          }
+          {
+            replace = "140000";
+            trigger = ";comp";
+          }
+          {
+            replace = "rtfd";
+            trigger = ";dob";
+          }
+          {
+            replace = "347JakeJanzen@gmail.com";
+            trigger = ";dem";
+          }
+          {
+            replace = "katiejanzen@347online.me";
+            trigger = ";pem";
+          }
+          {
+            replace = "Katie";
+            trigger = ";fn";
+          }
+          {
+            replace = "Katie Janzen";
+            trigger = ";n";
+          }
+          {
+            replace = "https://github.com/347Online";
+            trigger = ";gh";
+          }
+          {
+            replace = "Janzen";
+            trigger = ";ln";
+          }
+          {
+            replace = "https://www.linkedin.com/in/katie-janzen/";
+            trigger = ";li";
+          }
+          {
+            replace = "+1 (806) 654-6903";
+            trigger = ";mph";
+          }
+          {
+            replace = "1410 Ponderosa St Unit 231 Canyon, TX, 79015";
+            trigger = ";mail";
+          }
+          {
+            replace = "+1 (806) 654-6933";
+            trigger = ";ph";
+          }
+          {
+            replace = "rtfd";
+            trigger = ";ph";
+          }
+          {
+            replace = "she/her/hers";
+            trigger = ";pro";
+          }
+          {
+            replace = "1410 Ponderosa St";
+            trigger = ";sta";
+          }
+          {
+            replace = "Unit 231";
+            trigger = ";stu";
+          }
+          {
+            replace = "https://347online.me";
+            trigger = ";ws";
+          }
+          {
+            replace = "79015";
+            trigger = ";zc";
+          }
+          {
+            replace = "accessibility";
+            trigger = "a11y";
+          }
+          {
+            replace = "Text";
+            trigger = ":skull:";
+          }
+        ];
+      };
+    };
+
+  };
 }
