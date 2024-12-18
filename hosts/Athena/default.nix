d83e6ba3fc2843518398257666ca13822951de5e
347Online | Katie Janzen <katiejanzen@347online.me>
2024-12-17 20:25:03 -0600

    Remove telegram from Dock

--- hosts/Athena/default.nix
+++ hosts/Athena/default.nix
@@ -30,7 +30,6 @@
   darwin.dock.apps = [
     "/Applications/Overcast.app"
     "/System/Applications/Messages.app"
-    "/Applications/Telegram.app"
     "/System/Applications/Mail.app"
     "/Applications/TeamTalk5.app"
   ];
