a3ef19fc52f44b7c184364ccebd4948a4a4d79ca
347Online | Katie Janzen <katiejanzen@347online.me>
2025-01-11 05:24:34 -0600

    Move assertions into root of gaming module
    Add Heroic

--- modules/shared/games/default.nix
+++ modules/shared/games/default.nix
@@ -1,5 +1,24 @@
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
   imports = [
     ./minecraft.nix
   ];
+
+  config = lib.mkIf config.shared.gaming.enable {
+    assertions = [
+      {
+        assertion = config.shared.personal.enable;
+        message = "Gaming can only be enabled on personal devices.";
+      }
+    ];
+
+    home.packages = with pkgs; [
+      heroic
+    ];
+  };
 }
--- modules/shared/games/minecraft.nix
+++ modules/shared/games/minecraft.nix
@@ -5,11 +5,5 @@
   ...
 }:
 lib.mkIf config.shared.gaming.enable {
-  assertions = [
-    {
-      assertion = config.shared.personal.enable;
-      message = "Gaming can only be enabled on personal devices.";
-    }
-  ];
   home.packages = with pkgs; [ prismlauncher ];
 }
