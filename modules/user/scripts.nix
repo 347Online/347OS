6998cc85b42292ecda627196d3a729b22d6a1ab9
347Online | Katie Janzen <katiejanzen@347online.me>
2025-01-26 13:30:46 -0600

    New shell command: newgit
    Create a new directory, opens it and initializes a new git repo, similar to cargo new
    
    Reformat scripts.nix

--- modules/user/scripts.nix
+++ modules/user/scripts.nix
@@ -1,17 +1,18 @@
 { pkgs, flakeDir, ... }:
 let
+  scripts = [
+    (pkgs.writeShellScriptBin "newgit" ''
+      mkdir $1 && (cd $1 && git init)
+    '')
 
-  secedit = (
-    pkgs.writeShellScriptBin "secedit" ''
+    (pkgs.writeShellScriptBin "secedit" ''
       cd ${flakeDir}
       if sops secrets.yaml; then
         git commit secrets.yaml -m "Secrets"
       fi
-    ''
-  );
+    '')
 
-  eds = (
-    pkgs.writeShellScriptBin "eds" ''
+    (pkgs.writeShellScriptBin "eds" ''
       usage () {
         echo "Usage: eds [OPTIONS] [FILES...]
         [OPTIONS]
@@ -23,54 +24,51 @@ let
         "
       }
 
-        session="$(basename $PWD)"
+      session="$(basename $PWD)"
 
-        while getopts ":s:h" opt; do
-          case $opt in
-            s)
-              session=$OPTARG
-              ;;
-            h)
-              usage
-              exit 0
-              ;;
-            \?)
-              echo "Bad option: -$OPTARG" >&2
-              exit 1
-              ;;
-            :)
-              echo "Option -$OPTARG requires value" >&2
-              exit 1
-              ;;
-          esac
-        done
-        shift $((OPTIND - 1))
+      while getopts ":s:h" opt; do
+        case $opt in
+          s)
+            session=$OPTARG
+            ;;
+          h)
+            usage
+            exit 0
+            ;;
+          \?)
+            echo "Bad option: -$OPTARG" >&2
+            exit 1
+            ;;
+          :)
+            echo "Option -$OPTARG requires value" >&2
+            exit 1
+            ;;
+        esac
+      done
+      shift $((OPTIND - 1))
 
-        if tmux has-session -t "$session" 2>/dev/null; then
-          echo "Session" "$session" "already exists, attaching to it"
-        else
-          tmux new-session -d -s "$session"
-          tmux rename-window -t 1 "editor"
+      if tmux has-session -t "$session" 2>/dev/null; then
+        echo "Session" "$session" "already exists, attaching to it"
+      else
+        tmux new-session -d -s "$session"
+        tmux rename-window -t 1 "editor"
+        tmux new-window
+        tmux rename-window -t 2 "scratch"
+        tmux split-window -t "scratch" -v
+        tmux resize-pane -Z
+        if ! [[ $PWD == "${flakeDir}"* ]]; then
           tmux new-window
-          tmux rename-window -t 2 "scratch"
-          tmux split-window -t "scratch" -v
-          tmux resize-pane -Z
-          if ! [[ $PWD == "${flakeDir}"* ]]; then
-            tmux new-window
-            tmux rename-window -t 3 "347OS"
-            tmux send-keys -t "347OS" 'cd "${flakeDir}"' C-m 'clear' C-m
-          fi
-          tmux select-window -t "editor"
-          tmux send-keys -t "editor" "nvim $@" C-m
+          tmux rename-window -t 3 "347OS"
+          tmux send-keys -t "347OS" 'cd "${flakeDir}"' C-m 'clear' C-m
         fi
+        tmux select-window -t "editor"
+        tmux send-keys -t "editor" "nvim $@" C-m
+      fi
 
-        tmux attach-session -t "$session"
-    ''
-  );
+      tmux attach-session -t "$session"
+    '')
+  ];
 in
 {
-  home.packages = [
-    secedit
-    eds
-  ];
+  home.packages = scripts;
 }
