5ff0b1c58789f511f769342be86090620a146dfc
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-05 13:02:54 -0600

    Syncthing: Enable auth

--- modules/user/programs/syncthing.nix
+++ modules/user/programs/syncthing.nix
@@ -1,6 +1,17 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  homeDirectory,
+  ...
+}:
 {
   services.syncthing = {
     enable = lib.mkIf config.user.personal.enable true;
+
+    passwordFile = builtins.toPath "${homeDirectory}/.secrets/syncthing-gui-passwd.txt";
+
+    settings = {
+      gui.user = "katie";
+    };
   };
 }
