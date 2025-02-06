ad779f4fce1e8f28a99f945193a90415738f7d4e
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-06 16:33:29 -0600

    Syncthing: Add iPhone as sync target

--- modules/user/programs/syncthing.nix
+++ modules/user/programs/syncthing.nix
@@ -17,6 +17,7 @@
         Amber.id = "F3IMTHP-MIKJMWJ-SPUYHG2-CL4TWES-IHGWO2N-4SOHCOH-ZP2H4ST-FTNB4A4";
         Aspen.id = "3ORETYE-Q4EE3XR-TEK646E-4GQXSI5-CUVN3AF-LCSCJY5-KCOBPJ4-DPPT2AK";
         Athena.id = "BG4POXA-BQIWLZV-7VKGOFX-T7DU2MQ-Q7A6ARE-Z7XHCTO-E3UWLNH-Y3Y4RQM";
+        iPhone.id = "QIVADHQ-RRWEEOP-XDJDDLP-HQOPLKD-CJCVHQF-VJOOO44-JTGUZRM-GCID7AZ";
       };
 
       folders = {
@@ -25,6 +26,7 @@
             "Amber"
             "Aspen"
             "Athena"
+            "iPhone"
           ];
         };
       };
