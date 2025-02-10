7c9838b04ea47616d7eee3169c0548642c2c458e
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-09 23:25:57 -0600

    Accounts: Experimental Contacts and Calendar sync
    Restructure to share userName, passwordCommand, and single mkIf

--- modules/user/accounts.nix
+++ modules/user/accounts.nix
@@ -1,22 +1,43 @@
 { lib, config, ... }:
-{
-  accounts.email = {
-    accounts.Personal = lib.mkIf config.user.personal.enable {
-      realName = "Katie Janzen";
-      userName = "katie@347online.me";
-      address = "katie@347online.me";
-      aliases = [ "katiejanzen@347online.me" ];
-      primary = true;
-      flavor = "fastmail.com";
-      passwordCommand = "op read 'op://Katie/Fastmail/App Password'";
+lib.mkIf config.user.personal.enable (
+  let
+    userName = "katie@347online.me";
+    passwordCommand = "op read 'op://Katie/Fastmail/App Password'";
+  in
+  {
+    accounts = {
+      calendar.accounts.Personal = {
+        primary = true;
+        remote = {
+          inherit userName passwordCommand;
+          type = "caldav";
+          url = "caldav.fastmail.com";
+        };
+      };
+      contact.accounts.Personal = {
+        primary = true;
+        remote = {
+          inherit userName passwordCommand;
+          type = "carddav";
+          url = "carddav.fastmail.com";
+        };
+      };
+      email.accounts.Personal = {
+        inherit userName passwordCommand;
+        realName = "Katie Janzen";
+        address = "katie@347online.me";
+        aliases = [ "katiejanzen@347online.me" ];
+        primary = true;
+        flavor = "fastmail.com";
 
-      thunderbird = {
-        enable = true;
+        thunderbird = {
+          enable = true;
 
-        settings = id: {
-          "mail.smtpserver.smtp_${id}.authMethod" = 10;
+          settings = id: {
+            "mail.smtpserver.smtp_${id}.authMethod" = 10;
+          };
         };
       };
     };
-  };
-}
+  }
+)
