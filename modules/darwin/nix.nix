fa65d56e60b7c9edd13e55e17df18ecc1175f1bf
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-16 02:21:45 -0600

    Darwin: Increase Download Buffer Size

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -8,7 +8,10 @@
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = "nix-command flakes";
+    settings = {
+      experimental-features = "nix-command flakes";
+      download-buffer-size = 524312500;
+    };
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   };
 
