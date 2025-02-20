3dbb0f754a928a235071fb2e6eb0565a4ed42bc4
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-20 00:48:44 -0600

    Firefox: Nixify extension prefs
    Allow in private browsing and on restricted sites for all trusted
    extensions

--- modules/user/programs/gui/firefox.nix
+++ modules/user/programs/gui/firefox.nix
@@ -9,11 +9,14 @@
 let
   profilePath =
     if pkgs.stdenv.isLinux then
-      "${homeDirectory}/.mozilla/firefox/katie/"
+      "${homeDirectory}/.mozilla/firefox/katie"
     else
-      "${homeDirectory}/Library/Application Support/Firefox/Profiles/katie/";
-  ext._1password = "d634138d-c276-4fc8-924b-40a0ea21d284";
-  extList = (builtins.attrNames ext) ++ [
+      "${homeDirectory}/Library/Application Support/Firefox/Profiles/katie";
+  ext = {
+    _1password = "{d634138d-c276-4fc8-924b-40a0ea21d284}";
+    ublock-origin = "uBlock0@raymondhill.net";
+  };
+  extList = (builtins.attrValues ext) ++ [
     "addons-search-detection@mozilla.com"
     "default-theme@mozilla.org"
     "formautofill@mozilla.org"
@@ -25,7 +28,19 @@ let
 in
 lib.mkIf config.user.gui.enable {
   # TODO: Extension preferences
-  # home.file."${profilePath}/extensions-preferences.json".text = builtins.toJSON { };
+  home.file."${profilePath}/extensions-preferences.json".text = builtins.toJSON (
+    lib.mergeAttrsList (
+      builtins.map (x: {
+        ${x} = {
+          permissions = builtins.filter (x: x != null) [
+            "internal:privateBrowsingAllowed"
+            (if lib.hasInfix "@mozilla" x then "internal:svgContextPropertiesAllowed" else null)
+          ];
+          origins = [ ];
+        };
+      }) extList
+    )
+  );
 
   programs.firefox = {
     enable = true;
@@ -62,55 +77,6 @@ lib.mkIf config.user.gui.enable {
           };
         };
       };
-      settings =
-        let
-        in
-        {
-          "browser.aboutConfig.showWarning" = false;
-          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
-          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
-          "browser.startup.homepage_override.mstone" = "ignore";
-          "browser.startup.page" = 3;
-          "browser.tabs.inTitlebar" = 1;
-          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-          "browser.warnOnQuitShortcut" = false;
-          "datareporting.policy.dataSubmissionPolicyBypassNotification" = true;
-          "identity.sync.tokenserver.uri" = "https://firefox.fatgirl.cloud/1.0/sync/1.5";
-          "signon.rememberSignons" = false;
-
-          # TODO: Programmatically for all relevant extensions
-          "extensions.quarantineIgnoredByUser.{${ext._1password}}" = true;
-
-          "browser.uiCustomization.state" = builtins.toJSON rec {
-            placements = {
-              widget-overflow-fixed-list = [ ];
-              unified-extensions-area = [
-                "ublock0_raymondhill_net-browser-action"
-              ];
-              nav-bar = [
-                "back-button"
-                "forward-button"
-                "stop-reload-button"
-                "urlbar-container"
-                "downloads-button"
-                "_${ext._1password}_-browser-action"
-                "addon_darkreader_org-browser-action"
-              ];
-              toolbar-menubar = [
-                "menubar-items"
-              ];
-              TabsToolbar = [
-                "tabbrowser-tabs"
-                "new-tab-button"
-                "alltabs-button"
-              ];
-              vertical-tabs = [ ];
-              PersonalToolbar = [ "personal-bookmarks" ];
-            };
-            dirtyAreaCache = builtins.attrNames placements;
-            currentVersion = 20;
-          };
-        };
 
       # extensions = with config.nur.repos.rycee.firefox-addons; [
       #   darkreader
@@ -118,6 +84,57 @@ lib.mkIf config.user.gui.enable {
       #   ublock-origin
       #   # youtube-shorts-block
       # ];
+
+      settings = lib.mkMerge (
+        (builtins.map (x: {
+          "extensions.quarantineIgnoredByUser.${x}" = true;
+        }) extList)
+        ++ [
+          {
+            "browser.aboutConfig.showWarning" = false;
+            "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+            "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+            "browser.startup.homepage_override.mstone" = "ignore";
+            "browser.startup.page" = 3;
+            "browser.tabs.inTitlebar" = 1;
+            "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+            "browser.warnOnQuitShortcut" = false;
+            "datareporting.policy.dataSubmissionPolicyBypassNotification" = true;
+            "identity.sync.tokenserver.uri" = "https://firefox.fatgirl.cloud/1.0/sync/1.5";
+            "signon.rememberSignons" = false;
+
+            "browser.uiCustomization.state" = builtins.toJSON rec {
+              placements = {
+                widget-overflow-fixed-list = [ ];
+                unified-extensions-area = [
+                  "ublock0_raymondhill_net-browser-action"
+                ];
+                nav-bar = [
+                  "back-button"
+                  "forward-button"
+                  "stop-reload-button"
+                  "urlbar-container"
+                  "downloads-button"
+                  "_${ext._1password}_-browser-action"
+                  "addon_darkreader_org-browser-action"
+                ];
+                toolbar-menubar = [
+                  "menubar-items"
+                ];
+                TabsToolbar = [
+                  "tabbrowser-tabs"
+                  "new-tab-button"
+                  "alltabs-button"
+                ];
+                vertical-tabs = [ ];
+                PersonalToolbar = [ "personal-bookmarks" ];
+              };
+              dirtyAreaCache = builtins.attrNames placements;
+              currentVersion = 20;
+            };
+          }
+        ]
+      );
     };
   };
 }
