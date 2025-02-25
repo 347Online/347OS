23d476639af5d5a8c5f1603df7a049de778e6c56
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-25 14:37:12 -0600

    Espanso: Re-arrange secrets

--- modules/user/sops.nix
+++ modules/user/sops.nix
@@ -16,9 +16,6 @@
 
     secrets = {
       syncthing-gui-passwd = { };
-
-      "personal-info-expansions.yml".path =
-        "${homeDirectory}/.config/espanso/match/personal-info-expansions.yml";
     };
   };
 }
