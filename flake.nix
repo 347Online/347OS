f24e012d440016f867d8b1c0f54c703955c1088d
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-27 21:11:29 -0600

    Nix Flake Update
    Builds on: Amber

--- flake.nix
+++ flake.nix
@@ -46,8 +46,6 @@
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-      inputs.nix-darwin.follows = "nix-darwin";
-      inputs.home-manager.follows = "home-manager";
     };
 
     nvim-emmet = {
