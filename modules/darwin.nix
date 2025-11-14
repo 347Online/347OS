4093e72969f9c7c2df69df2c325d9dde638216d8
347Online | Katie Janzen <katiejanzen@347online.me>
2025-11-14 15:33:59 -0700

    Remove let-in for util in darwin hosts config

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -2,17 +2,14 @@
   config,
   ...
 }:
-let
-  util = config.flake.util;
-in
 {
   systems = [ "aarch64-darwin" ];
   flake = {
     darwinConfigurations = {
-      Athena = util.mkDarwin {
+      Athena = config.flake.util.mkDarwin {
         module = ./hosts/Athena.nix;
       };
-      Alice = util.mkDarwin {
+      Alice = config.flake.util.mkDarwin {
         module = ./hosts/Alice.nix;
         username = "kjanzen";
       };
