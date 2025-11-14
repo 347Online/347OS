5385bfb887442e8be4391a0482bb2dd71c7fb1ea
347Online | Katie Janzen <katiejanzen@347online.me>
2025-11-14 16:23:44 -0700

    Sketch out essentials moduleset

--- modules/essentials.nix
+++ modules/essentials.nix
@@ -1,26 +1,48 @@
-pkgs: with pkgs; [
-  bat
-  coreutils
-  eza
-  delta
-  direnv
-  fd
-  fzf
-  git
-  go
-  htop
-  moreutils
-  neovim
-  nixfmt-rfc-style
-  nix-search-cli
-  nodejs
-  ookla-speedtest
-  python3
-  ripgrep
-  rustup
-  trunk
-  screen
-  shellcheck
-  tmux
-  zoxide
-]
+let
+  mkEssentials =
+    pkgs: with pkgs; [
+      bat
+      coreutils
+      eza
+      delta
+      direnv
+      fd
+      fzf
+      git
+      go
+      htop
+      moreutils
+      neovim
+      nixfmt-rfc-style
+      nix-search-cli
+      nodejs
+      ookla-speedtest
+      python3
+      ripgrep
+      rustup
+      trunk
+      screen
+      shellcheck
+      tmux
+      zoxide
+    ];
+in
+{
+  flake = {
+    homeModules.essentials =
+      { pkgs, ... }:
+      {
+        home.packages = mkEssentials pkgs;
+      };
+    darwinModules.essentials =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = mkEssentials pkgs;
+      };
+    nixosModules.essentials =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = mkEssentials pkgs;
+      };
+  };
+}
