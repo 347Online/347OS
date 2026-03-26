1ad3901ef91bcbba7cae5e4ab1cb944e8ef7923e
347Online | Katie Janzen <katiejanzen@347online.me>
2026-03-26 06:39:13 -0500

    Flake: Remove extraneous gui check in gui module

--- modules/home/programs/gui/default.nix
+++ modules/home/programs/gui/default.nix
@@ -17,7 +17,7 @@
     ./thunderbird.nix
   ];
 
-  programs.ssh.extraConfig = lib.mkIf config.user.gui.enable (
+  programs.ssh.extraConfig = (
     util.mkIfElse pkgs.stdenv.isDarwin
       "IdentityAgent \"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
       "IdentityAgent ~/.1password/agent.sock"
