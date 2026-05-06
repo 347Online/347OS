c09a720a3ac0b9d1d0d2025f5b01d5be32c9b228
347Online | Katie Janzen <katiejanzen@347online.me>
2026-05-06 17:30:29 -0500

    Flake: Dendriticize Darwin Modules

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,73 +1,77 @@
+{ ... }:
 {
-  lib,
-  config,
-  username,
-  ...
-}:
-lib.mkIf config.darwin.homebrew.enable {
-  nix-homebrew = {
-    enable = true;
+  flake.darwinModules.homebrew =
+    {
+      lib,
+      config,
+      username,
+      ...
+    }:
+    lib.mkIf config.darwin.homebrew.enable {
+      nix-homebrew = {
+        enable = true;
 
-    user = username;
-    autoMigrate = true;
-  };
+        user = username;
+        autoMigrate = true;
+      };
 
-  homebrew = {
-    enable = true;
+      homebrew = {
+        enable = true;
 
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
-      upgrade = true;
+        onActivation = {
+          autoUpdate = true;
+          cleanup = "zap";
+          upgrade = true;
+        };
+
+        brews = [
+          # "mas"
+        ];
+
+        # TODO: Uncomment after re-pinning to 26.05
+        # masApps = {
+        #   "1Password for Safari" = 1569813296;
+        #   "Broadcasts" = 1469995354;
+        #   "Dark Reader for Safari" = 1438243180;
+        #   "Fantastical" = 975937182;
+        #   # "Fantastical" = "com.flexibits.fantastical2.mac"; # TODO: PR nix-darwin to support bundle ids
+        #   "Magic Lasso" = 1198047227;
+        #   "Keynote" = 361285480;
+        #   "Numbers" = 361304891;
+        #   # "Overcast" = 888422857; # Currently broken
+        #   "Pages" = 361309726;
+        #   "StopTheMadness Pro" = 6471380298;
+        # };
+
+        casks = [
+          "1password"
+          "firefox"
+          "ghostty"
+          "google-chrome"
+          "logi-options+"
+          "monitorcontrol"
+          "obsidian"
+          "raycast"
+          "scroll-reverser"
+          "setapp"
+          "whatcable"
+        ]
+        ++ lib.optionals config.darwin.gaming.enable [
+          "multipatch"
+          "openemu"
+          "prismlauncher"
+        ];
+
+      };
+
+      environment.variables = {
+        HOMEBREW_NO_ENV_HINTS = "1";
+        HOMEBREW_PREFIX = "/opt/homebrew";
+        HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
+        HOMEBREW_REPOSITORY = "/opt/homebrew/Library/.homebrew-is-managed-by-nix";
+        PATH = "/opt/homebrew/bin:/opt/homebrew/sbin\${PATH+:$PATH}";
+        MANPATH = "/opt/homebrew/share/man\${MANPATH+:$MANPATH}:";
+        INFOPATH = "/opt/homebrew/share/info:\${INFOPATH:-}";
+      };
     };
-
-    brews = [
-      # "mas"
-    ];
-
-    # TODO: Uncomment after re-pinning to 26.05
-    # masApps = {
-    #   "1Password for Safari" = 1569813296;
-    #   "Broadcasts" = 1469995354;
-    #   "Dark Reader for Safari" = 1438243180;
-    #   "Fantastical" = 975937182;
-    #   # "Fantastical" = "com.flexibits.fantastical2.mac"; # TODO: PR nix-darwin to support bundle ids
-    #   "Magic Lasso" = 1198047227;
-    #   "Keynote" = 361285480;
-    #   "Numbers" = 361304891;
-    #   # "Overcast" = 888422857; # Currently broken
-    #   "Pages" = 361309726;
-    #   "StopTheMadness Pro" = 6471380298;
-    # };
-
-    casks = [
-      "1password"
-      "firefox"
-      "ghostty"
-      "google-chrome"
-      "logi-options+"
-      "monitorcontrol"
-      "obsidian"
-      "raycast"
-      "scroll-reverser"
-      "setapp"
-      "whatcable"
-    ]
-    ++ lib.optionals config.darwin.gaming.enable [
-      "multipatch"
-      "openemu"
-      "prismlauncher"
-    ];
-
-  };
-
-  environment.variables = {
-    HOMEBREW_NO_ENV_HINTS = "1";
-    HOMEBREW_PREFIX = "/opt/homebrew";
-    HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
-    HOMEBREW_REPOSITORY = "/opt/homebrew/Library/.homebrew-is-managed-by-nix";
-    PATH = "/opt/homebrew/bin:/opt/homebrew/sbin\${PATH+:$PATH}";
-    MANPATH = "/opt/homebrew/share/man\${MANPATH+:$MANPATH}:";
-    INFOPATH = "/opt/homebrew/share/info:\${INFOPATH:-}";
-  };
 }
