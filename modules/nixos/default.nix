ba9533bedb5e4f110e5378f71614a24c83f79e2c
347Online | Katie Janzen <katiejanzen@347online.me>
2026-08-09 10:58:35 -0600

    Amber: Remove redundant config / setup comments

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -45,6 +45,8 @@
             # ];
           };
 
+          time.timeZone = "America/Denver";
+
           users.users.${username} = {
             isNormalUser = true;
             shell = pkgs.zsh;
@@ -61,6 +63,22 @@
           programs.gnupg.agent = {
             enable = true;
             enableSSHSupport = true;
+          };
+
+          i18n = {
+            defaultLocale = "en_US.UTF-8";
+
+            extraLocaleSettings = {
+              LC_ADDRESS = "en_US.UTF-8";
+              LC_IDENTIFICATION = "en_US.UTF-8";
+              LC_MEASUREMENT = "en_US.UTF-8";
+              LC_MONETARY = "en_US.UTF-8";
+              LC_NAME = "en_US.UTF-8";
+              LC_NUMERIC = "en_US.UTF-8";
+              LC_PAPER = "en_US.UTF-8";
+              LC_TELEPHONE = "en_US.UTF-8";
+              LC_TIME = "en_US.UTF-8";
+            };
           };
 
           networking.networkmanager.enable = true;
